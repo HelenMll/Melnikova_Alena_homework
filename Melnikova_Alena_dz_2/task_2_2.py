833933109d1e4221fb688958d5e3c647e1f0f617
HelenMll <89741428+HelenMll@users.noreply.github.com>
2021-10-17 21:35:36 +0300

    The second part was done

---
+++ Melnikova_Alena_dz_2/task_2_2.py
@@ -0,0 +1,14 @@
+start_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+proof = id(start_list)
+
+for items_order, items in enumerate(start_list):
+    if items.isdigit() == True:  # вычленяем целые числа
+        start_list[items_order] = f' "{int(items):02d}" '  # заменяем полностью элемент списка
+    elif items[0] == '+' and items[1:].isdigit() == True:  # вычленяем числа с +
+        start_list[items_order] = f' "+{int(items):02d}" '
+    elif items[0] == '-' and items[1:].isdigit() == True:  # вычленяем числа с -
+        start_list[items_order] = f' "{int(items):03d}" '  # 03d т.к - тоже считается за элемент
+    print(start_list[items_order], end=' ')
+
+if id(start_list) == proof:
+    print('\n', 'Список не был изменен.')
